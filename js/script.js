930d2b2fe8f1c27b2e85852f0bc3b1105e56568d
Lucas Ferro <dibbu.ferro@gmail.com>
2022-10-30 12:36:53 -0300

    formulario, apiRest y mucho mas

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ let cards = document.querySelectorAll('.cardItem');
     card.classList.toggle('is-flipped');
   });
 });
+
+//DEBIDO AL CONCEPTO DEL SITIO (VIAJES FUTURISTAS EN EL UNIVERSO DE FUTURAMA " SERIE DE TELEVISION "), NO ENCONTRE UNA API REST QUE
+//SEA VALIDA PARA EL CONCEPTO DEL SITIO. AUN ASI DEJO ACA LA LLAMADA A UNA API REST.
+//API REST UTILIZADA " FAKESTOREAPI "
+
+let products=[]
+fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=> products.push(json))
